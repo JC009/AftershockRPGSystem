7348dbbb9bfc41c16ec4b43fecaebc1c46327c26
JC09 <32849971+JC009@users.noreply.github.com>
2020-01-06 22:11:20 +0100

    Added ammoList
    and some other minor tests

---
+++ app/src/main/java/com/example/aftershockrpgsystem/ammo/ammoList.kt
@@ -0,0 +1,373 @@
+package com.example.aftershockrpgsystem.ammo
+
+class ammoList {
+
+    class MM9()
+    {
+        private val prcShort = 0
+        private val prcMedium = -2
+        private val prcLong = -6
+
+        private val apShort = 2
+        private val apMedium = 2
+        private val apLong = 0
+
+        private val dmgShort = 3
+        private val dmgMedium = 3
+        private val dmgLong = 2
+
+        val stats = listOf(
+            prcShort, prcMedium, prcLong, apShort, apMedium, apLong, dmgShort, dmgMedium, dmgLong )
+    }
+
+    class ACP45()
+    {
+        private val prcShort = 0
+        private val prcMedium = -3
+        private val prcLong = -7
+
+        private val apShort = 2
+        private val apMedium = 2
+        private val apLong = 0
+
+        private val dmgShort = 4
+        private val dmgMedium = 4
+        private val dmgLong = 2
+
+        val stats = listOf(
+            prcShort, prcMedium, prcLong, apShort, apMedium, apLong, dmgShort, dmgMedium, dmgLong )
+    }
+
+    class MM10()
+    {
+        private val prcShort = 0
+        private val prcMedium = -2
+        private val prcLong = -6
+
+        private val apShort = 2
+        private val apMedium = 2
+        private val apLong = 0
+
+        private val dmgShort = 4
+        private val dmgMedium = 4
+        private val dmgLong = 2
+
+        val stats = listOf(
+            prcShort, prcMedium, prcLong, apShort, apMedium, apLong, dmgShort, dmgMedium, dmgLong )
+    }
+
+    class AE50()
+    {
+        private val prcShort = 0
+        private val prcMedium = -2
+        private val prcLong = -5
+
+        private val apShort = 4
+        private val apMedium = 4
+        private val apLong = 1
+
+        private val dmgShort = 8
+        private val dmgMedium = 7
+        private val dmgLong = 5
+
+        val stats = listOf(
+            prcShort, prcMedium, prcLong, apShort, apMedium, apLong, dmgShort, dmgMedium, dmgLong )
+    }
+
+    class MAG357()
+    {
+        private val prcShort = 0
+        private val prcMedium = -2
+        private val prcLong = -6
+
+        private val apShort = 3
+        private val apMedium = 3
+        private val apLong = 1
+
+        private val dmgShort = 5
+        private val dmgMedium = 5
+        private val dmgLong = 4
+
+        val stats = listOf(
+            prcShort, prcMedium, prcLong, apShort, apMedium, apLong, dmgShort, dmgMedium, dmgLong )
+    }
+
+    class MAG44()
+    {
+        private val prcShort = 0
+        private val prcMedium = -2
+        private val prcLong = -6
+
+        private val apShort = 4
+        private val apMedium = 4
+        private val apLong = 2
+
+        private val dmgShort = 6
+        private val dmgMedium = 6
+        private val dmgLong = 5
+
+        val stats = listOf(
+            prcShort, prcMedium, prcLong, apShort, apMedium, apLong, dmgShort, dmgMedium, dmgLong )
+    }
+
+    class MAG500()
+    {
+        private val prcShort = 0
+        private val prcMedium = -2
+        private val prcLong = -5
+
+        private val apShort = 5
+        private val apMedium = 5
+        private val apLong = 2
+
+        private val dmgShort = 10
+        private val dmgMedium = 9
+        private val dmgLong = 8
+
+        val stats = listOf(
+            prcShort, prcMedium, prcLong, apShort, apMedium, apLong, dmgShort, dmgMedium, dmgLong )
+    }
+
+    class MM556()
+    {
+        private val prcShort = 0
+        private val prcMedium = 0
+        private val prcLong = -1
+
+        private val apShort = 4
+        private val apMedium = 4
+        private val apLong = 4
+
+        private val dmgShort = 4
+        private val dmgMedium = 4
+        private val dmgLong = 4
+
+        val stats = listOf(
+            prcShort, prcMedium, prcLong, apShort, apMedium, apLong, dmgShort, dmgMedium, dmgLong )
+    }
+
+    class GRENDEL()
+    {
+        private val prcShort = 0
+        private val prcMedium = 0
+        private val prcLong = -3
+
+        private val apShort = 4
+        private val apMedium = 4
+        private val apLong = 3
+
+        private val dmgShort = 5
+        private val dmgMedium = 5
+        private val dmgLong = 5
+
+        val stats = listOf(
+            prcShort, prcMedium, prcLong, apShort, apMedium, apLong, dmgShort, dmgMedium, dmgLong )
+    }
+
+    class MM762()
+    {
+        private val prcShort = 0
+        private val prcMedium = 0
+        private val prcLong = -1
+
+        private val apShort = 5
+        private val apMedium = 5
+        private val apLong = 5
+
+        private val dmgShort = 5
+        private val dmgMedium = 5
+        private val dmgLong = 5
+
+        val stats = listOf(
+            prcShort, prcMedium, prcLong, apShort, apMedium, apLong, dmgShort, dmgMedium, dmgLong )
+    }
+
+    class BLACKOUT()
+    {
+        private val prcShort = 0
+        private val prcMedium = 0
+        private val prcLong = -4
+
+        private val apShort = 4
+        private val apMedium = 3
+        private val apLong = 3
+
+        private val dmgShort = 4
+        private val dmgMedium = 4
+        private val dmgLong = 3
+
+        val stats = listOf(
+            prcShort, prcMedium, prcLong, apShort, apMedium, apLong, dmgShort, dmgMedium, dmgLong )
+    }
+
+    class WINMAG()
+    {
+        private val prcShort = 0
+        private val prcMedium = 0
+        private val prcLong = 0
+
+        private val apShort = 7
+        private val apMedium = 7
+        private val apLong = 7
+
+        private val dmgShort = 10
+        private val dmgMedium = 10
+        private val dmgLong = 10
+
+        val stats = listOf(
+            prcShort, prcMedium, prcLong, apShort, apMedium, apLong, dmgShort, dmgMedium, dmgLong )
+    }
+
+    class LAPMAG()
+    {
+        private val prcShort = 0
+        private val prcMedium = 0
+        private val prcLong = 0
+
+        private val apShort = 8
+        private val apMedium = 8
+        private val apLong = 8
+
+        private val dmgShort = 14
+        private val dmgMedium = 14
+        private val dmgLong = 14
+
+        val stats = listOf(
+            prcShort, prcMedium, prcLong, apShort, apMedium, apLong, dmgShort, dmgMedium, dmgLong )
+    }
+
+    class BMG()
+    {
+        private val prcShort = 0
+        private val prcMedium = 0
+        private val prcLong = 0
+
+        private val apShort = 15
+        private val apMedium = 15
+        private val apLong = 15
+
+        private val dmgShort = 30
+        private val dmgMedium = 30
+        private val dmgLong = 30
+
+        val stats = listOf(
+            prcShort, prcMedium, prcLong, apShort, apMedium, apLong, dmgShort, dmgMedium, dmgLong )
+    }
+
+    class GAUGE12LEAD()
+    {
+        private val prcShort = 2
+        private val prcMedium = -2
+        private val prcLong = -7
+
+        private val apShort = 1
+        private val apMedium = 1
+        private val apLong = 0
+
+        private val dmgShort = 2
+        private val dmgMedium = 2
+        private val dmgLong = 2
+
+        private val shotNumberShort = 6
+        private val shotNumberMedium = 4
+        private val shotNumberLong = 2
+
+        val stats = listOf(
+            prcShort, prcMedium, prcLong, apShort, apMedium, apLong,
+            dmgShort, dmgMedium, dmgLong, shotNumberShort, shotNumberMedium, shotNumberLong )
+    }
+
+    class GAUGE12STEEL()
+    {
+        private val prcShort = 2
+        private val prcMedium = -2
+        private val prcLong = -7
+
+        private val apShort = 2
+        private val apMedium = 2
+        private val apLong = 1
+
+        private val dmgShort = 2
+        private val dmgMedium = 2
+        private val dmgLong = 2
+
+        private val shotNumberShort = 6
+        private val shotNumberMedium = 4
+        private val shotNumberLong = 2
+
+        val stats = listOf(
+            prcShort, prcMedium, prcLong, apShort, apMedium, apLong,
+            dmgShort, dmgMedium, dmgLong, shotNumberShort, shotNumberMedium, shotNumberLong )
+    }
+
+    class GAUGE12SLUG()
+    {
+        private val prcShort = 0
+        private val prcMedium = 0
+        private val prcLong = -4
+
+        private val apShort = 3
+        private val apMedium = 2
+        private val apLong = 0
+
+        private val dmgShort = 11
+        private val dmgMedium = 9
+        private val dmgLong = 6
+
+        private val shotNumberShort = 1
+        private val shotNumberMedium = 1
+        private val shotNumberLong = 1
+
+        val stats = listOf(
+            prcShort, prcMedium, prcLong, apShort, apMedium, apLong,
+            dmgShort, dmgMedium, dmgLong, shotNumberShort, shotNumberMedium, shotNumberLong )
+    }
+
+    class GAUGE12DRAGON()
+    {
+        private val prcShort = 3
+        private val prcMedium = -2
+        private val prcLong = 0
+
+        private val apShort = 0
+        private val apMedium = 0
+        private val apLong = 0
+
+        private val dmgShort = 10
+        private val dmgMedium = 5
+        private val dmgLong = 0
+
+        private val shotNumberShort = 1
+        private val shotNumberMedium = 1
+        private val shotNumberLong = 0
+
+        val stats = listOf(
+            prcShort, prcMedium, prcLong, apShort, apMedium, apLong,
+            dmgShort, dmgMedium, dmgLong, shotNumberShort, shotNumberMedium, shotNumberLong )
+    }
+
+    class GAUGE12BIRDSHOT()
+    {
+        private val prcShort = 3
+        private val prcMedium = -4
+        private val prcLong = 0
+
+        private val apShort = 0
+        private val apMedium = 0
+        private val apLong = 0
+
+        private val dmgShort = 1
+        private val dmgMedium = 1
+        private val dmgLong = 0
+
+        private val shotNumberShort = 9
+        private val shotNumberMedium = 4
+        private val shotNumberLong = 0
+
+        val stats = listOf(
+            prcShort, prcMedium, prcLong, apShort, apMedium, apLong,
+            dmgShort, dmgMedium, dmgLong, shotNumberShort, shotNumberMedium, shotNumberLong )
+    }
+
+
+}
