f41afff9c1b59bc2cb068c5bbe339f33cdb87628
JC09 <32849971+JC009@users.noreply.github.com>
2020-01-09 00:23:56 +0100

    Some changes to dice

--- app/src/main/java/com/example/aftershockrpgsystem/dice_simulator/DiceActivity.kt
+++ app/src/main/java/com/example/aftershockrpgsystem/dice_simulator/DiceActivity.kt
@@ -9,8 +9,12 @@ import com.example.aftershockrpgsystem.R
 
 import kotlinx.android.synthetic.main.activity_dice.*
 import kotlinx.android.synthetic.main.content_dice.*
+import kotlinx.android.synthetic.main.dice_dialog.*
+import kotlin.random.Random
 
 class DiceActivity : AppCompatActivity() {
+
+    public var dialogText = "0"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,13 +22,45 @@ class DiceActivity : AppCompatActivity() {
         setContentView(R.layout.activity_dice)
         setSupportActionBar(toolbar)
 
-        button_dice.setOnClickListener {
-            showDialog(it)
+        button_d4.setOnClickListener {
+            showDialog(it, 4)
+        }
+
+        button_d6.setOnClickListener {
+            showDialog(it, 6)
+        }
+
+        button_d8.setOnClickListener {
+            showDialog(it, 8)
+        }
+
+        button_d10.setOnClickListener {
+            showDialog(it, 10)
+        }
+
+        button_d12.setOnClickListener {
+            showDialog(it, 12)
+        }
+        button_d20.setOnClickListener {
+            showDialog(it, 20)
+        }
+
+        button_d100.setOnClickListener {
+            showDialog(it, 100)
         }
     }
 
-    private fun showDialog (view: View)
+    private fun randomNumber (i: Int): Int
     {
+        return Random.nextInt(1,i+1)
+    }
+
+    private fun showDialog (view: View, dice: Int)
+    {
+        val number = randomNumber(dice)
+
+
+
         val builder : AlertDialog.Builder = AlertDialog.Builder(this)
         val inflater : LayoutInflater = layoutInflater
         val view : View = inflater.inflate(R.layout.dice_dialog, null)
@@ -47,5 +83,4 @@ class DiceActivity : AppCompatActivity() {
         val dialog : AlertDialog = builder.create()
         dialog.show()
     }
-
 }
