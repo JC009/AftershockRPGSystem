964e62609f60f873e3dccfacb307fd4f36d1daac
JC09 <32849971+JC009@users.noreply.github.com>
2020-01-09 23:00:43 +0100

    Rules started
    Added slick nav_view

--- app/src/main/java/com/example/aftershockrpgsystem/MainActivity.kt
+++ app/src/main/java/com/example/aftershockrpgsystem/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import com.example.aftershockrpgsystem.calculator.CalculatorActivity
 import com.example.aftershockrpgsystem.dice_simulator.DiceActivity
+import com.example.aftershockrpgsystem.rules.RulesActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_rules.setOnClickListener {
+            val intent = Intent(this, RulesActivity::class.java)
+            startActivity(intent)
+        }
 
         button_calculator.setOnClickListener {
             // Handler code here.
